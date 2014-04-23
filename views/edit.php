f4abfd3b61c004aabe554c80d0514c72d48dffcb
seven <seven@geovoyagers.de>
2014-04-23 23:05:58 +0200

    Removed ":" and "." to fit to new en-message.php

--- views/edit.php
+++ views/edit.php
@@ -20,9 +20,9 @@ $allowed_filetypes = str_replace(',',', ',Plugin::getSetting('filetypes','downlo
 		<?php if (isset($id)) echo "<a href=\"/download-file/{$id}/{$filename}\" target=\"_blank\">{$filename}</a>"; ?><br />
 		<table class="fieldset" cellpadding="0" cellspacing="0" border="0">
 			<tr>
-				<td class="label"><label for="download"><?php echo __('Download:');?> </label></td>
+				<td class="label"><label for="download"><?php echo __('Download');?> </label></td>
 				<td class="field"><input name="download" id="download" type="file" /></td>
-				<td class="help"><?php echo __('Allowed filetypes').': '.$allowed_filetypes; ?></td>
+				<td class="help"><?php echo __('Allowed filetypes').$allowed_filetypes; ?></td>
 			</tr>
 		</table>
 	</fieldset>
@@ -31,32 +31,32 @@ $allowed_filetypes = str_replace(',',', ',Plugin::getSetting('filetypes','downlo
 		<legend style="padding: 0em 0.5em 0em 0.5em; font-weight: bold;"><?php echo __('Information'); ?></legend>
 		<table class="fieldset" cellpadding="0" cellspacing="0" border="0">
 			<tr>
-				<td class="label"><label for="name"><?php echo __('Name:');?> </label></td>
+				<td class="label"><label for="name"><?php echo __('Name');?> </label></td>
 				<td class="field"><input name="name" id="name" type="text" size="35" maxsize="255" value="<?php echo isset($name) ? $name : '';?>"/></td>
 				<td class="help"><?php echo __('The download name; be descriptive'); ?></td>
 			</tr>
 			<tr>
-				<td class="label"><label for="description"><?php echo __('Description:');?> </label></td>
+				<td class="label"><label for="description"><?php echo __('Description');?> </label></td>
 				<td class="field"><textarea name="description" id="description" rows="4" cols="35"><?php echo isset($description) ? $description : '';?></textarea></td>
 				<td class="help"><?php echo __('Describe the download; visible to the public'); ?></td>
 			</tr>
 			<tr>
-				<td class="label"><label for="keywords"><?php echo __('Key Words:');?> </label></td>
+				<td class="label"><label for="keywords"><?php echo __('Key Words');?> </label></td>
 				<td class="field"><input name="keywords" id="keywords" type="text" size="35" maxsize="255" value="<?php echo isset($keywords) ? $keywords : '';?>" /></td>
 				<td class="help"><?php echo __('Key words for search; separate with spaces'); ?></td>
 			</tr>
 			<tr>
-				<td class="label"><label for="tags"><?php echo __('Tags:');?> </label></td>
+				<td class="label"><label for="tags"><?php echo __('Tags');?> </label></td>
 				<td class="field"><input name="tags" id="tags" type="text" size="35" maxsize="255" value="<?php echo isset($tags) ? $tags : '';?>"/></td>
-				<td class="help"><?php echo __('Seperate with commas.'); ?></td>
+				<td class="help"><?php echo __('Seperate with commas'); ?></td>
 			</tr>
 			<tr>
-				<td class="label"><label for="expires"><?php echo __('Expires Date:');?> </label></td>
+				<td class="label"><label for="expires"><?php echo __('Expires Date');?> </label></td>
 				<td class="field"><input name="expires" id="expires" type="text" size="35" maxsize="255" value="<?php echo isset($expires) ? $expires : '';?>"/></td>
 				<td class="help"><?php echo __('Use MM/DD/YYYY format'); ?></td>
 			</tr>
 			<tr>
-				<td class="label"><label for="active"><?php echo __('Active:');?> </label></td>
+				<td class="label"><label for="active"><?php echo __('Active');?> </label></td>
 				<td class="field"><input name="active" id="active" type="checkbox" value="1" <?php echo $active ? 'checked="checked"' : '';?> /></td>
 				<td class="help"><?php echo __('Is the download active?'); ?></td>
 			</tr>
@@ -76,7 +76,7 @@ function setConfirmUnload(on, msg) {
 }
 
 function unloadMessage() {
-	return '<?php echo __('You have modified this page.  If you navigate away from this page without first saving your data, the changes will be lost.'); ?>';
+	return '<?php echo __('You have modified this page. If you navigate away from this page without first saving your data, the changes will be lost.'); ?>';
 }
 
 $(document).ready(function() {
